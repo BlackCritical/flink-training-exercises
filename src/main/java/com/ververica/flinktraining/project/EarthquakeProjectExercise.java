1ec48fc54044ce1eb61bedefd6bd49bf6b1f70a9
Leander Nachtmann <leander.nachtmann@proximity.de>
2019-10-17 19:38:00 +0200

    added filter

--- src/main/java/com/ververica/flinktraining/project/EarthquakeProjectExercise.java
+++ src/main/java/com/ververica/flinktraining/project/EarthquakeProjectExercise.java
@@ -3,10 +3,14 @@ package com.ververica.flinktraining.project;
 import com.ververica.flinktraining.exercises.datastream_java.utils.ExerciseBase;
 import com.ververica.flinktraining.project.model.Earthquake;
 import com.ververica.flinktraining.project.model.Feature;
+import com.ververica.flinktraining.project.model.Geometry;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -52,9 +56,12 @@ public class EarthquakeProjectExercise extends ExerciseBase {
 //		DataStream<Feature> rides = env.addSource(new EarthquakeSource(input, maxEventDelay, servingSpeedFactor));
 		DataStream<Feature> rides = env.fromCollection(earthquake.features);
 
-		DataStream<Feature> filteredRides = rides
+		DataStream<Tuple2<Feature, Integer>> filteredRides = rides
 				// filter out rides that do not start or stop in NYC
-				.filter(new NYCFilter());
+				.filter(new LocationFilter())
+				.flatMap(new CountAssigner())
+				.keyBy(1)
+				.sum(1);
 
 		// print the filtered stream
 		printOrTest(filteredRides);
@@ -63,11 +70,25 @@ public class EarthquakeProjectExercise extends ExerciseBase {
 		env.execute("Taxi Ride Cleansing");
 	}
 
-	private static class NYCFilter implements FilterFunction<Feature> {
+	private static class LocationFilter implements FilterFunction<Feature> {
 		@Override
 		public boolean filter(Feature value) throws Exception {
-			return true;
+			Geometry geometry = value.geometry;
+			if (geometry.type.equalsIgnoreCase("Point") && !geometry.coordinates.isEmpty()) {
+				double longitude = geometry.coordinates.get(0);
+				double latitude = geometry.coordinates.get(1);
+//				double depth = geometry.coordinates.get(2);
+
+				return longitude > 0 && latitude > 0;
+			}
+			return false;
 		}
 	}
 
+	private static class CountAssigner implements FlatMapFunction<Feature, Tuple2<Feature, Integer>> {
+		@Override
+		public void flatMap(Feature value, Collector<Tuple2<Feature, Integer>> out) throws Exception {
+			out.collect(new Tuple2<>(value, 1));
+		}
+	}
 }
